081a08b43a5f13f6e71e039cc361389d9c4296ac
Akwesi-Bona <arhinbonnah@gmail.com>
2024-01-23 22:43:03 +0000

    error fixed

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-""" Provides stats about Nginx logs restored in mongoDB"""
-import pymongo as pm
-db = pm.MongoClient()
-mydb = db["logs"]
-mycol = mydb["nginx"]
-
+""" MongoDB Operations with Python using pymongo """
+from pymongo import MongoClient
 
 if __name__ == "__main__":
-    get_get = mycol.count_documents({"method": "GET"})
-    get_post = mycol.count_documents({"method": "POST"})
-    get_put = mycol.count_documents({"method": "PUT"})
-    get_patch = mycol.count_documents({"method": "PATCH"})
-    get_delete = mycol.count_documents({"method": "DELETE"})
-    get_total = mycol.count_documents({})
-    get_status = mycol.count_documents({"method": "GET", "path": "/status"})
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-    print("{} logs".format(get_total))
-    print("Methods:\n\tmethod GET: {}\n\tmethod POST: {}\n\tmethod PUT: {}\n\tmethod PATCH: {}\n\tmethod DELETE: {}".format(
-        get_get, get_post, get_put, get_patch, get_delete))
-    print("{} status check".format(get_status))
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
