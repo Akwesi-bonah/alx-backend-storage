8708c73d322c7c667b64b1f10911377527342420
Akwesi-Bona <arhinbonnah@gmail.com>
2024-01-23 17:06:42 +0000

    task 11 error fixed(method name corrected 2)

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Defines school_by_topics methods"""
 
-def school_by_topic(mongo_collection, topic):
+def schools_by_topic(mongo_collection, topic):
     """Return list of school having a specific topic"""
     topics_list = {"topics": {"$elemMatch": {"$eq": topic,},},}
     return [i for i in mongo_collection.find(topics_list)]
